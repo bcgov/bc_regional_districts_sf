6eb5aa6348d30041adc07016353ffe1a626daf93
rich <rich.p.martin@gmail.com>
2022-07-02 18:10:38 -0700

    small changes and added gimp file.

--- 01_load.R
+++ 01_load.R
@@ -12,53 +12,62 @@
 
 library(tidyverse)
 
-rd <- sf::st_read(here::here("data","rd-polygon.shp"))[-c(33,35),]%>%
-  mutate(id=row_number(),
-         regional_district=c(
-           "capital",
-           "cowichan",
-           "alberni_clayoquot",
-           "nanaimo",
-           "metro_vancouver",
-           "fraser_valley",
-           "okanagan_similkameen",
-           "kootenay_boundary",
-           "central_kootenay",
-           "sunshine_coast",
-           "comox_valley",
-           "east_kootenay",
-           "strathcona",
-           "squamish_lillooet",
-           "qathet",
-           "thompson_nicola",
-           "central_okanagan",
-           "mount_waddington",
-           "strathcona",
-           "north_okanagan",
-           "mount_waddington",
-           "columbia_shuswap",
-           "cariboo",
-           "central_coast",
-           "north_coast",
-           "kitamat_stikine",
-           "fraser_fort_george",
-           "bulkley_nechako",
-           "north_coast",
-           "north_coast",
-           "peace_river",
-           "stikine",
-           "northern_rockies"
-         ))
+rd <- sf::st_read(here::here("data", "rd-polygon.shp"))[-c(33, 35), ] %>%
+  mutate(
+    id = row_number(),
+    regional_district = c(
+      "capital",
+      "cowichan",
+      "alberni_clayoquot",
+      "nanaimo",
+      "metro_vancouver",
+      "fraser_valley",
+      "okanagan_similkameen",
+      "kootenay_boundary",
+      "central_kootenay",
+      "sunshine_coast",
+      "comox_valley",
+      "east_kootenay",
+      "strathcona",
+      "squamish_lillooet",
+      "qathet",
+      "thompson_nicola",
+      "central_okanagan",
+      "mount_waddington",
+      "strathcona",
+      "north_okanagan",
+      "mount_waddington",
+      "columbia_shuswap",
+      "cariboo",
+      "central_coast",
+      "north_coast",
+      "kitamat_stikine",
+      "fraser_fort_george",
+      "bulkley_nechako",
+      "north_coast",
+      "north_coast",
+      "peace_river",
+      "stikine",
+      "northern_rockies"
+    )
+  )
 
-write_rds(rd, here::here("out","bc_regional_districts_sf.rds"))
+write_rds(rd, here::here("out", "bc_regional_districts_sf.rds"))
 
-# example usage---------
-plt <- ggplot(rd)+
-  geom_sf(aes(fill=regional_district),lwd=0)+
-  theme_void()+
+# download sf file from github---------
+
+bc_regional_districts <- read_rds("https://github.com/bcgov/bc_regional_districts_sf/raw/main/out/bc_regional_districts_sf.rds")
+
+#example usage----------
+
+plt <- ggplot(bc_regional_districts) +
+  geom_sf(aes(fill = regional_district), lwd = 0) +
+  theme_void() +
   theme(legend.position = "none")
 
-plotly::ggplotly(plt)%>%
+plotly::ggplotly(plt) %>%
   plotly::style(hoveron = "fill")
+
+
 
 
